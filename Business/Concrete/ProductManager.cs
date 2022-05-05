939c0bda667c880d82cdf9ad39513743c5713f24
Hakan YİĞİT <hakanyigit028@hotmail.com>
2022-05-06 00:53:28 +0300

    6 Mayıs

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -54,7 +54,7 @@ namespace Business.Concrete
         [CacheAspect] //key ,value 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 13) 
+            if (DateTime.Now.Hour == 5) 
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
